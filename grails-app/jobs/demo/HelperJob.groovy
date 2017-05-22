5971be4ea9c01e2af12177395858a464e5d58209
Jeff Scott Brown <brownj@objectcomputing.com>
2017-05-22 15:47:32 -0500

    remove @Autowired

--- grails-app/jobs/demo/HelperJob.groovy
+++ grails-app/jobs/demo/HelperJob.groovy
@@ -1,13 +1,10 @@
 package demo
 
-import org.springframework.beans.factory.annotation.Autowired
-
 class HelperJob {
     static triggers = {
       simple repeatInterval: 5000l
     }
 
-    @Autowired
     SomeBean someBean
 
     def execute() {
